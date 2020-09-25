8edd4fc204f3758ed36fe237d36fc0636ef0a374
glassjoseph <glassjoseph@gmail.com>
2020-09-24 21:06:43 -0400

    Fix Exceptions\Handler for Laravel 6
    - fix exception handler
    - tweak composer.json
    - remove middleware auth for beta testing

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,11 @@ use App\Concept;
 use Illuminate\Http\Request;
 
 Route::get('concepts',              'ConceptController@index');
-Route::get('concepts/create',       'ConceptController@create')->middleware('auth');
-Route::post('concepts',             'ConceptController@store')->middleware('auth');
-Route::post('concepts/{concept}/add_term', 'ConceptController@addTerm')->middleware('auth');
+Route::get('concepts/create',       'ConceptController@create');
+Route::post('concepts',             'ConceptController@store');
+Route::post('concepts/{concept}/add_term', 'ConceptController@addTerm');
 Route::get('concepts/{concept}',    'ConceptController@show');
-Route::delete('concepts/{concept}', 'ConceptController@destroy')->middleware('auth');
+Route::delete('concepts/{concept}', 'ConceptController@destroy');
 
 Route::get('login/github', 'Auth\LoginController@redirectToProvider');
 Route::get('github/login', 'Auth\LoginController@handleProviderCallback');
