0f0eab99785ee295102955a6017dd97a9487dc33
Nevin Morgan <nevin@longtowel.com>
2024-08-20 09:34:57 -0400

    Add editable term component and example term list

---
+++ resources/js/components/Term/Editable.vue
@@ -0,0 +1,64 @@
+<template>
+  <BInputGroup>
+    <BFormInput
+      type="text"
+      :required="true"
+      v-model="term.text"
+      @input="updateTerm"
+    ></BFormInput>
+
+    <BInputGroupAppend>
+      <BButton @click="emitSaveTerm" class="btn btn-info" title="Make Preferred"
+        ><i class="fa fa-floppy-o"></i
+      ></BButton>
+      <BButton
+        @click="emitMakeTermPreferred"
+        class="btn btn-primary"
+        title="Make Preferred"
+        ><i class="fa fa-check-square-o"></i
+      ></BButton>
+      <BButton @click="emitDeleteTerm" class="btn btn-danger" title="Delete"
+        ><i class="fa fa-trash"></i
+      ></BButton>
+    </BInputGroupAppend>
+  </BInputGroup>
+</template>
+
+<script>
+import {
+  BButton,
+  BFormInput,
+  BInputGroup,
+  BInputGroupAppend,
+} from 'bootstrap-vue';
+
+export default {
+  model: {
+    prop: 'term',
+    event: 'input',
+  },
+  props: {
+    term: Object,
+  },
+  components: {
+    BInputGroup,
+    BInputGroupAppend,
+    BFormInput,
+    BButton,
+  },
+  methods: {
+    updateTerm(event) {
+      this.$emit('input', { ...this.term, text: event.target.value });
+    },
+    emitSaveTerm() {
+      this.$emit('save-term', this.term);
+    },
+    emitMakeTermPreferred() {
+      this.$emit('make-term-preferred', this.term);
+    },
+    emitDeleteTerm() {
+      this.$emit('delete-term', this.term);
+    },
+  },
+};
+</script>
---
+++ resources/js/components/Term/List.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-for="term in terms" v-bind:key="term.id">
+      <p v-if="!editing">{{ term.text }}</p>
+      <Editable v-else :term="term"></Editable>
+    </div>
+  </div>
+</template>
+
+<script>
+import Editable from './Editable.vue';
+
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    editing: Boolean,
+    conceptId: Number,
+    terms: {
+      type: Array,
+    },
+  },
+  components: {
+    Editable,
+  },
+};
+</script>
