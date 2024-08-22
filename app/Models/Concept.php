f7269fc9022d8bf6c445b457a4cd119161ccfa29
Nevin Morgan <nevinsm@users.noreply.github.com>
2024-08-22 10:08:47 -0400

    [31419] clean up vocabulary concept creation form

--- app/Models/Concept.php
+++ app/Models/Concept.php
@@ -8,7 +8,7 @@ class Concept extends Model
 {
     protected $fillable = [
         "deprecated",
-        "concept"
+        "concept",
     ];
 
     protected $hidden = ["created_at", "updated_at"];
@@ -20,58 +20,65 @@ class Concept extends Model
      */
     protected $with = ["terms"];
 
-    public function replacementOf() {
+    public function replacementOf()
+    {
         return $this->hasOne("\App\Models\Concept", "deprecated_to");
     }
 
-    public function deprecatedTo() {
+    public function deprecatedTo()
+    {
         return $this->belongsTo("\App\Models\Concept", "deprecated_to");
     }
 
-    public function setDeprecatedTo($concept) {
+    public function setDeprecatedTo($concept)
+    {
         $this->deprecated = true;
         return $this->deprecatedTo()->associate($concept)
             ->save();
     }
 
-    public function terms() {
+    public function terms()
+    {
         return $this->hasMany("App\Models\Term");
     }
 
-    public function broader() {
+    public function broader()
+    {
         return $this->belongsToMany("App\Models\Concept", "concept_relationships", "concept_id", "related_concept_id")
-                        ->withPivot("relationship_type")
-                        ->wherePivot("relationship_type", "broader");
+            ->withPivot("relationship_type")
+            ->wherePivot("relationship_type", "broader");
     }
 
     // This version requires insert of the two relationships (both ways, narrower and broader)
     //public function narrower() {
-        //return $this->belongsToMany("App\Models\Concept", "concept_relationships", "concept_id", "related_concept_id")
-                        //->withPivot("relationship_type")
-                        //->wherePivot("relationship_type", "narrower");
+    //return $this->belongsToMany("App\Models\Concept", "concept_relationships", "concept_id", "related_concept_id")
+    //->withPivot("relationship_type")
+    //->wherePivot("relationship_type", "narrower");
     //}
-
 
     // This version requires insert only one relationships, the other relationship is inferred from concept_id or related_concept_id
     // coming from other direction
-     public function narrower() {
-         return $this->belongsToMany("App\Models\Concept", "concept_relationships", "related_concept_id", "concept_id")
-                         ->withPivot("relationship_type")
-                         ->wherePivot("relationship_type", "broader");
-     }
-
-
-    public function related() {
+    public function narrower()
+    {
         return $this->belongsToMany("App\Models\Concept", "concept_relationships", "related_concept_id", "concept_id")
-                        ->withPivot("relationship_type")
-                        ->wherePivot("relationship_type", "related");
+            ->withPivot("relationship_type")
+            ->wherePivot("relationship_type", "broader");
     }
 
-    public function addBroader($conceptId) {
+    public function related()
+    {
+        return $this->belongsToMany("App\Models\Concept", "concept_relationships", "related_concept_id", "concept_id")
+            ->withPivot("relationship_type")
+            ->wherePivot("relationship_type", "related");
+    }
+
+    public function addBroader($conceptId)
+    {
         return $this->broader()->attach([$conceptId => ["relationship_type" => "broader"]]);
     }
 
-    public function addRelated($conceptId) {
+    public function addRelated($conceptId)
+    {
         return $this->related()->attach([$conceptId => ["relationship_type" => "related"]]);
     }
 
@@ -81,26 +88,30 @@ class Concept extends Model
     //     return $this->narrower()->attach([$concept => ["relationship_type" => "narrower"]]);
     // }
 
-
-    public function addNarrower($conceptId) {
+    public function addNarrower($conceptId)
+    {
         // TODO : check it works for new narrower enum
         return $this->narrower()->attach([$conceptId => ["relationship_type" => "broader"]]);
     }
 
-    public function sources() {
+    public function sources()
+    {
         return $this->hasMany("App\Models\ConceptSource");
     }
 
-    public function conceptProperties() {
+    public function conceptProperties()
+    {
         return $this->hasMany("App\Models\ConceptProperty");
     }
 
-    public function conceptCategories() {
+    public function conceptCategories()
+    {
         return $this->belongsToMany("App\Models\Vocabulary", "concept_categories", "concept_id", "category_id");
     }
 
     // TODO: Make a preferredTerm function that returns correct term?
-    public function preferredTerm() {
+    public function preferredTerm()
+    {
         return $this->hasOne("App\Models\Term")->where("preferred", true)->first();
     }
 
