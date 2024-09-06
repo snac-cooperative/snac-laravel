73ff2afc34cab8fb62712af6f377a53af085fc33
Nevin Morgan <nevin@longtowel.com>
2024-09-06 17:01:09 -0400

    shift categories constant to config file

--- resources/js/components/Concept/Create.vue
+++ resources/js/components/Concept/Create.vue
@@ -54,6 +54,7 @@
 
 <script>
 import { BFormGroup, BButton, BFormInput, BFormSelect } from 'bootstrap-vue';
+import { categories } from '../../config/catgegories';
 
 export default {
   data() {
@@ -61,14 +62,7 @@ export default {
       preferredTerm: null,
       conceptId: null,
       saved: false,
-      categories: [
-        { value: process.env.MIX_ETHNICITY_ID, text: 'Ethnicity' },
-        { value: process.env.MIX_OCCUPATION_ID, text: 'Occupation' },
-        { value: process.env.MIX_ACTIVITY_ID, text: 'Activity' },
-        { value: process.env.MIX_SUBJECT_ID, text: 'Subject' },
-        { value: process.env.MIX_RELIGION_ID, text: 'Religion' },
-        { value: process.env.MIX_RELATION_ID, text: 'Relation' },
-      ],
+      categories,
       categoryId: null,
       baseURL: process.env.MIX_APP_URL,
     };
--- resources/js/components/ConceptCategory.vue
+++ resources/js/components/ConceptCategory.vue
@@ -1,39 +1,34 @@
 <template>
-    <div id="concept-table" classname="">
-        <div class="form-group">
-            <div class="mt-3">
-                <h4>Concept Category</h4>
-                <b-col cols="4">
-                    <b-form-select v-model="selectedCategory" :options="categories"></b-form-select>
-                </b-col>
-            </div>
-        </div>
+  <div id="concept-table" classname="">
+    <div class="form-group">
+      <div class="mt-3">
+        <h4>Concept Category</h4>
+        <b-col cols="4">
+          <b-form-select
+            v-model="selectedCategory"
+            :options="categories"
+          ></b-form-select>
+        </b-col>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-// import TermItem from './TermItem.vue';
-    export default {
-        props: {
-            categoriesProps: {
-                type: Array
-            }
+import { categories } from '../config/catgegories';
 
-        },
-        data() {
-            return {
-                categories: [
-                    { value:  process.env.MIX_ETHNICITY_ID, text: 'Ethnicity'},
-                    { value:  process.env.MIX_OCCUPATION_ID, text: 'Occupation'},
-                    { value:  process.env.MIX_ACTIVITY_ID, text: 'Activity'},
-                    { value:  process.env.MIX_SUBJECT_ID, text: 'Subject'},
-                    { value:  process.env.MIX_RELIGION_ID, text: 'Religion'},
-                    { value:  process.env.MIX_RELATION_ID, text: 'Relation'},
-                ],
-            }
-        }
-    }
+export default {
+  props: {
+    categoriesProps: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      categories,
+    };
+  },
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
