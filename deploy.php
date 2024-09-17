2891c213091c96bf4c48635ee39c7b5595502c05
Jeff Mueller <jeff.mueller@simplethread.com>
2024-09-17 15:14:31 -0400

    One last tweak to creating the ENV file

--- deploy.php
+++ deploy.php
@@ -40,7 +40,7 @@ task('deploy:secrets', function () {
     }
     $env_file = ($stage == 'production') ? 'ENV_PROD' : 'ENV_DEV';
     file_put_contents(__DIR__ . '/.env', base64_decode(getenv($env_file)));
-    upload('.env', get('deploy_path') . '/shared');
+    upload(__DIR__ . '/.env', get('deploy_path') . '/shared');
 });
 
 task('build', function() {
