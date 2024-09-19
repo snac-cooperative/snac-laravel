2d80a642310ed15d5c620ffe14a8de6bf9a92fbb
Nevin Morgan <nevin@longtowel.com>
2024-09-19 13:13:58 -0400

    initial sanctum setup and concept api controller protection

--- app/Http/Controllers/API/ConceptController.php
+++ app/Http/Controllers/API/ConceptController.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\DB;
 class ConceptController extends Controller
 {
     /**
+     * Instantiate a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except(['index', 'show']);
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @param  \Illuminate\Http\Request  $request
