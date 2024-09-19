00b281bcf18311780395544762656510ce64e9ab
Nevin Morgan <nevin@longtowel.com>
2024-09-19 11:16:28 -0400

    remove barrel file for api service layer

--- resources/js/api/index.js
+++
@@ -1,5 +0,0 @@
-import ConceptService from './ConceptService';
-import ConceptSourceService from './ConceptSourceService';
-import TermService from './TermService';
-
-export { ConceptService, ConceptSourceService, TermService };
