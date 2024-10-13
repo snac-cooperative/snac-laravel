5b053b61de2f32edd0ec265470d94e0a12321f6e
Nevin Morgan <nevin@longtowel.com>
2024-10-13 06:00:28 -0400

    [#34722] Scaffold out Concepts Authorization

--- app/Http/Controllers/API/ConceptController.php
+++ app/Http/Controllers/API/ConceptController.php
@@ -21,6 +21,7 @@ class ConceptController extends Controller
     public function __construct()
     {
         $this->middleware('auth:sanctum')->except(['index', 'show', 'reconcile']);
+        $this->authorizeResource(Concept::class, 'concepts');
     }
 
     /**
---
+++ tests/Feature/API/ConceptsTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Feature\API;
+
+use Tests\TestCase;
+
+class ConceptsTest extends TestCase
+{
+    public function test_any_can_list_concepts(): void
+    {
+
+    }
+
+    public function test_any_can_search_concepts(): void
+    {
+
+    }
+
+    public function test_any_can_get_concept(): void
+    {
+
+    }
+
+    public function test_authorized_user_can_create_concept(): void
+    {
+    }
+
+    public function test_authorized_user_can_update_concept(): void
+    {
+
+    }
+
+    public function test_authorized_user_can_update_concept_relationships(): void
+    {
+
+    }
+
+    public function test_authorized_user_can_deprecate_concept(): void
+    {
+
+    }
+
+    public function test_authorized_user_can_delete_concept(): void
+    {
+
+    }
+
+    public function test_unauthorized_user_cannot_create_concept(): void
+    {
+
+    }
+
+    public function test_unauthorized_user_cannot_update_concept(): void
+    {
+
+    }
+
+    public function test_unauthorized_user_cannot_update_concept_relationships(): void
+    {
+
+    }
+
+    public function test_unauthorized_user_cannot_deprecate_concept(): void
+    {
+
+    }
+
+    public function test_unauthorized_user_cannot_delete_concept(): void
+    {
+
+    }
+}
