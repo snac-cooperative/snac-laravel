523f06751e25b970709741dd85ff29cb9604f6a1
Nevin Morgan <nevin@longtowel.com>
2024-10-21 16:58:16 -0400

    feat: Add ConceptSources API testing

---
+++ app/Policies/ConceptSourcePolicy.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\ConceptSource;
+use App\Models\User;
+
+class ConceptSourcePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(?User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(?User $user, ConceptSource $source): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->isVocabularyEditor();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, ConceptSource $source): bool
+    {
+        return $user->isVocabularyEditor();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, ConceptSource $source): bool
+    {
+        return $user->isVocabularyEditor();
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, ConceptSource $source): bool
+    {
+        return $user->isVocabularyEditor();
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, ConceptSource $source): bool
+    {
+        return $user->isVocabularyEditor();
+    }
+}
--- tests/Feature/API/ConceptSourcesTest.php
+++ tests/Feature/API/ConceptSourcesTest.php
@@ -5,31 +5,31 @@ namespace Tests\Feature\API;
 use App\Models\Concept;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use App\Models\Term;
+use App\Models\ConceptSource;
 use App\Models\Role;
 use App\Models\User;
 use Tests\TestCase;
 use Laravel\Sanctum\Sanctum;
 
-class TermsTest extends TestCase
+class ConceptSourcesTest extends TestCase
 {
     use DatabaseTransactions;
     use WithFaker;
 
-    public function test_any_can_list_terms(): void
+    public function test_any_can_list_concept_sources(): void
     {
-        $response = $this->getJson('/api/terms');
+        $response = $this->getJson('/api/concept_sources');
         $response->assertStatus(200);
     }
 
-    public function test_any_can_get_term(): void
+    public function test_any_can_get_concept_source(): void
     {
-        $term = Term::first();
-        $response = $this->getJson("/api/terms/{$term->id}");
+        $conceptSource = ConceptSource::factory()->create();
+        $response = $this->getJson("/api/concept_sources/{$conceptSource->id}");
         $response->assertStatus(200);
     }
 
-    public function test_authorized_user_can_create_term(): void
+    public function test_authorized_user_can_create_concept_source(): void
     {
         $reviewerRole = Role::whereHas('permissions', function ($query) {
             $query->where('label', 'Edit Vocabulary');
@@ -38,16 +38,18 @@ class TermsTest extends TestCase
         Sanctum::actingAs($user);
 
         $concept = Concept::factory()->create();
-        $response = $this->postJson('/api/terms', [
+        $response = $this->postJson('/api/concept_sources', [
+            'citation' => $this->faker->sentence(),
             'concept_id' => $concept->id,
-            'preferred' => false,
-            'text' => $this->faker->word,
+            'found_data' => $this->faker->sentence(),
+            'note' => $this->faker->paragraph(),
+            'url' => $this->faker->url(),
         ]);
 
         $response->assertStatus(201);
     }
 
-    public function test_authorized_user_can_update_term(): void
+    public function test_authorized_user_can_update_concept_source(): void
     {
         $reviewerRole = Role::whereHas('permissions', function ($query) {
             $query->where('label', 'Edit Vocabulary');
@@ -55,16 +57,18 @@ class TermsTest extends TestCase
         $user = User::factory()->hasAttached($reviewerRole)->create();
         Sanctum::actingAs($user);
 
-        $term = Term::factory()->create();
-        $response = $this->patchJson("/api/terms/{$term->id}", [
-            'text' => $this->faker->word
+        $conceptSource = ConceptSource::factory()->create();
+        $response = $this->patchJson("/api/concept_sources/{$conceptSource->id}", [
+            'citation' => $this->faker->sentence(),
+            'found_data' => $this->faker->sentence(),
+            'note' => $this->faker->paragraph(),
+            'url' => $this->faker->url(),
         ]);
 
-        $this->assertNotEquals(Term::find($term->id)->text, $term->text);
         $response->assertStatus(200);
     }
 
-    public function test_authorized_user_can_delete_term(): void
+    public function test_authorized_user_can_delete_concept_source(): void
     {
         $reviewerRole = Role::whereHas('permissions', function ($query) {
             $query->where('label', 'Edit Vocabulary');
@@ -72,13 +76,13 @@ class TermsTest extends TestCase
         $user = User::factory()->hasAttached($reviewerRole)->create();
         Sanctum::actingAs($user);
 
-        $term = Term::factory()->create();
-        $response = $this->deleteJson("/api/terms/{$term->id}");
+        $conceptSource = ConceptSource::factory()->create();
+        $response = $this->deleteJson("/api/concept_sources/{$conceptSource->id}");
 
         $response->assertStatus(204);
     }
 
-    public function test_unauthorized_user_cannot_create_term(): void
+    public function test_unauthorized_user_cannot_create_concept_source(): void
     {
         $nonReviewerRole = Role::whereDoesntHave('permissions', function ($query) {
             $query->where('label', 'Edit Vocabulary');
@@ -87,16 +91,18 @@ class TermsTest extends TestCase
         Sanctum::actingAs($user);
 
         $concept = Concept::factory()->create();
-        $response = $this->postJson('/api/terms', [
+        $response = $this->postJson('/api/concept_sources', [
+            'citation' => $this->faker->sentence(),
             'concept_id' => $concept->id,
-            'preferred' => false,
-            'text' => $this->faker->word,
+            'found_data' => $this->faker->sentence(),
+            'note' => $this->faker->paragraph(),
+            'url' => $this->faker->url(),
         ]);
 
         $response->assertStatus(403);
     }
 
-    public function test_unauthorized_user_cannot_update_term(): void
+    public function test_unauthorized_user_cannot_update_concept_source(): void
     {
         $nonReviewerRole = Role::whereDoesntHave('permissions', function ($query) {
             $query->where('label', 'Edit Vocabulary');
@@ -104,15 +110,18 @@ class TermsTest extends TestCase
         $user = User::factory()->hasAttached($nonReviewerRole)->create();
         Sanctum::actingAs($user);
 
-        $term = Term::factory()->create();
-        $response = $this->patchJson("/api/terms/{$term->id}", [
-            'text' => $this->faker->word
+        $conceptSource = ConceptSource::factory()->create();
+        $response = $this->patchJson("/api/concept_sources/{$conceptSource->id}", [
+            'citation' => $this->faker->sentence(),
+            'found_data' => $this->faker->sentence(),
+            'note' => $this->faker->paragraph(),
+            'url' => $this->faker->url(),
         ]);
 
         $response->assertStatus(403);
     }
 
-    public function test_unauthorized_user_cannot_delete_term(): void
+    public function test_unauthorized_user_cannot_delete_concept_source(): void
     {
         $nonReviewerRole = Role::whereDoesntHave('permissions', function ($query) {
             $query->where('label', 'Edit Vocabulary');
@@ -120,8 +129,8 @@ class TermsTest extends TestCase
         $user = User::factory()->hasAttached($nonReviewerRole)->create();
         Sanctum::actingAs($user);
 
-        $term = Term::factory()->create();
-        $response = $this->deleteJson("/api/terms/{$term->id}");
+        $conceptSource = ConceptSource::factory()->create();
+        $response = $this->deleteJson("/api/concept_sources/{$conceptSource->id}");
 
         $response->assertStatus(403);
     }
