1abda918d207d22de72bbcf297d1a992c997e372
jglass-st <joseph.glass@simplethread.com>
2024-12-19 09:05:37 -0500

    Add deprecateConcept()

--- resources/js/components/Concept/mixins/Deprecate.js
+++ resources/js/components/Concept/mixins/Deprecate.js
@@ -2,5 +2,22 @@ import ConceptService from '../../../api/ConceptService';
 
 export default {
   methods: {
+    async deprecateConcept() {
+      console.log('Deprecating concept', this.selectedConcept);
+      if (!confirm('This action cannot be undone. Are you sure you want to deprecate?')) {
+        return;
+      }
+      const [error, data] = await ConceptService.deprecateConcept(
+        this.conceptId,
+        this.selectedConcept.id,
+      );
+
+      if (error) {
+        console.error('Failed to deprecate concept:', error);
+        return;
+      }
+      this.flashSuccessAlert();
+      window.location.reload();
+    },
   },
 };
